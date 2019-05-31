61734382d554b29534371d8977caa78e3429de4a
Nelson Osacky <nelson.osacky@soundcloud.com>
2019-05-31 14:56:02 +0200

    Remove boilerplate with HasAndroidInjector.

--- app/src/main/java/com/osacky/daggerupgradeapplication/DaggerExampleApplication.kt
+++ app/src/main/java/com/osacky/daggerupgradeapplication/DaggerExampleApplication.kt
@@ -1,33 +1,21 @@
 package com.osacky.daggerupgradeapplication
 
-import android.app.Activity
 import android.app.Application
-import android.app.Service
-import android.content.BroadcastReceiver
-import androidx.fragment.app.Fragment
 import dagger.android.AndroidInjector
 import dagger.android.DispatchingAndroidInjector
-import dagger.android.HasActivityInjector
-import dagger.android.HasBroadcastReceiverInjector
-import dagger.android.HasServiceInjector
-import dagger.android.support.HasSupportFragmentInjector
+import dagger.android.HasAndroidInjector
 import javax.inject.Inject
 
-class DaggerExampleApplication : Application(), HasActivityInjector, HasServiceInjector, HasBroadcastReceiverInjector, HasSupportFragmentInjector {
+class DaggerExampleApplication : Application(), HasAndroidInjector {
 
-    @Inject lateinit var androidActivityInjector : DispatchingAndroidInjector<Activity>
-    @Inject lateinit var androidFragmentInjector : DispatchingAndroidInjector<Fragment>
-    @Inject lateinit var androidBroadcastReceiverInjector : DispatchingAndroidInjector<BroadcastReceiver>
-    @Inject lateinit var androidServiceInjector : DispatchingAndroidInjector<Service>
+    @Inject lateinit var androidInjector : DispatchingAndroidInjector<Any>
 
     override fun onCreate() {
         super.onCreate()
 
-        DaggerExampleAppComponent.builder().build()
+        DaggerExampleAppComponent.create()
             .injectApplication(this)
     }
-    override fun activityInjector(): AndroidInjector<Activity> = androidActivityInjector
-    override fun serviceInjector(): AndroidInjector<Service> = androidServiceInjector
-    override fun broadcastReceiverInjector(): AndroidInjector<BroadcastReceiver> = androidBroadcastReceiverInjector
-    override fun supportFragmentInjector(): AndroidInjector<Fragment> = androidFragmentInjector
+
+    override fun androidInjector(): AndroidInjector<Any> = androidInjector
 }
